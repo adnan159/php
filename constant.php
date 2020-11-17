f4e993a7d63fe03fdb1f817318591efce31bce01
Osman Haider <osmanhaider159@gmail.com>
2020-11-18 04:27:50 +0600

    all file is commited for push

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+
+//declar a constant
+define('PI', 3.1416);
+
+$task = "Read";
+
+echo "$task the book";
+
+//print the constant
+echo "Value of PI = " .PI;
---
+++ plus_minus.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$number = 10;
+$number2 = 20;
+
+$number = $number+$number2;//30
+$number2= $number - $number2;//10
+$number = $number - $number2;
+printf("%s %s",$number,$number2);
+
+
