c83c064622eb15e32e07faa348e00132ad75e3dd
Osman Haider <osmanhaider159@gmail.com>
2020-11-19 07:55:42 +0600

    change in loop and ternary operator

--- ternary.php
+++ ternary.php
@@ -4,3 +4,8 @@ $n =13;
 
 $result = ($n % 2 == 0) ? "{$n} is a Even Number" : "{$n} is a odd number";
 echo $result;
+
+//
+
+$output = ($n % 2 == 0) ? "Even" : (($n == 11) ? "Odd" : "Nothing");
+echo "$output";
